4e2719c3d52aca765feb3fdb3a6a8607070a58f9
SoumayaMauthoorMOJ <soumaya.mauthoor@digital.justice.gov.uk>
2022-12-28 18:54:19 +0000

    update backend

--- infra/bucket.py
+++ infra/bucket.py
@@ -5,18 +5,18 @@ from pulumi_aws.s3 import (
 )
 
 config = Config()
-bucket_config = config.require_object("bucket")
+bucket_name = config.require("bucket")
 feature_flags = config.get_object("feature_flags", {})
 stack = get_stack()
 
 bucketWebsiteArgs = None
-if feature_flags.get("bucket_object"):
+if feature_flags.get("public_website"):
     bucketWebsiteArgs = BucketWebsiteArgs(
         index_document="index.html",
     )
 
-bucket = Bucket(f"{bucket_config['name']}-{stack}", website=bucketWebsiteArgs)
+bucket = Bucket(f"{bucket_name}-{stack}", website=bucketWebsiteArgs)
 
 export("bucket_name", bucket.id)
-if feature_flags.get("bucket_object"):
+if feature_flags.get("public_website"):
     export("bucket_endpoint", Output.concat("http://", bucket.website_endpoint))
